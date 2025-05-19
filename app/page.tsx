ac5fec201c1993b0c6b3bcb1c32c3c37f1cf1e39
Aprameya Kannan <101409453+aprameyak@users.noreply.github.com>
2025-05-19 01:26:32 -0400

    basic setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
 import Image from "next/image";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Header from "../components/Header"
+import { useRouter } from "next/router";
+import { Router } from "lucide-react";
 export default function Home() {
+  const router = useRouter()
+  const handleloggedout = () => {
+    router.push("/login");
+  }
   return (
     <div>
-      <Header/>
-      <body>
-      </body>
-
+      <SignedOut>
+        handleloggedout()
+      </SignedOut>
+      <SignedIn>
+      <div>
+        <Header/>
+        <body>
+        </body>
+      </div>
+      </SignedIn>
     </div>
   );
 }
