8dc8f0d47615349d82d3611e6191c938e644e487
Aprameya Kannan <101409453+aprameyak@users.noreply.github.com>
2025-05-19 01:18:39 -0400

    init

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+
+const Header = () => {
+    return (
+        <div className="flex row-start-auto">
+            <div className="flex">
+                Home
+            </div>
+            <div className="flex">
+                Favorites
+            </div>
+            <div className="flex">
+                Profile
+            </div>
+        </div>
+    );
+};
+
+export default Header;
